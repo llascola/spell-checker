efbed19273613690aadad1c29054762376fdeaa2
Luciano Scola <lucianoluigiscola@gmail.com>
2022-11-29 12:11:18 -0300

    Utils implementios / io

---
+++ Source/utils.c
@@ -0,0 +1,16 @@
+#include "utils.h"
+
+unsigned hash_len(char* wrd, int len) {
+	unsigned hashval;
+	for (int i = 0; i < len; i++)
+		hashval += wrd[i] + 31 * hashval;
+	return hashval;
+}
+
+unsigned hash(char* wrd){ 
+	unsigned hashval;
+	for (char* s = wrd; *s != 0; s++) {
+		hashval += *s + 31 * hashval;
+	}
+	return hashval;
+}
---
+++ Source/utils.h
@@ -0,0 +1,9 @@
+#ifndef __UTILS__
+#define __UTILS__	
+
+unsigned hash_len(char* wrd, int len);
+
+unsigned hash(char* wrd);
+
+
+#endif
